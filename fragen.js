92206113262c2244c894e37c0002ea036ca27ee9
pady-ai <patricia.birkenmeier@stud.fhgr.ch>
2024-02-07 08:44:31 +0100

    Festlegung der Antwortmöglichkeiten und und der richtigen Antwort

---
+++ fragen.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+        numb: 1, 
+        question: "Was ist ein Merkmal von Fake News?",
+        answer: "Sie verbreiten falsche oder irreführende Informationen.",
+        options: [
+            "A. Sie basieren immer auf Fakten.",
+            "B. Sie sind oft gut recherchiert.",
+            "C. Sie verbreiten falsche oder irreführende Informationen."
+
+        ]
+    },
+    {
+        numb: 2, 
+        question: "Welche Rolle spielen soziale Medien bei der Verbreitung von Informationen?",
+        answer: "Sie können Informationen schnell verbreiten, aber auch zur Verbreitung von Fehlinformationen beitragen.",
+        options: [
+            "A. Sie haben keinen Einfluss auf die Verbreitung von Informationen. ",
+            "B. Sie können Informationen schnell verbreiten, aber auch zur Verbreitung von Fehlinformationen beitragen.",
+            "C. Sie garantieren die Richtigkeit aller veröffentlichten Informationen. "
+
+        ]
+    },
+    {
+        numb: 3, 
+        question: "Warum ist es wichtig, Quellen zu überprüfen, bevor man Informationen weitergibt?",
+        answer: "Weil es wichtig ist, sicherzustellen, dass die Informationen korrekt und zuverlässig sind.",
+        options: [
+            "A. Weil es Spaß macht.",
+            "B. Weil es wichtig ist, sicherzustellen, dass die Informationen korrekt und zuverlässig sind.",
+            "C. Weil man sich dadurch beliebt macht."
+
+
+        ]
+    },
+    {
+        numb: 4, 
+        question: "Was ist ein Beispiel für eine vertrauenswürdige Nachrichtenquelle?",
+        answer: "Eine renommierte Zeitung mit gutem Ruf für objektive Berichterstattung.",
+        options: [
+            "A. Ein Blog, der keine Quellen angibt.",
+            "B. Eine Website, die oft Clickbait verwendet.",
+            "C. Eine renommierte Zeitung mit gutem Ruf für objektive Berichterstattung."
+            
+
+        ]
+    },
+    {
+        numb: 5, 
+        question: "Was bedeutet es, kritisch gegenüber Medieninhalten zu sein?",
+        answer: "Informationen und Quellen sorgfältig zu prüfen und zu hinterfragen",
+        options: [
+            "A. Alles zu glauben, was man liest.",
+            "B. Informationen und Quellen sorgfältig zu prüfen und zu hinterfragen.",
+            "C. Sich nur auf eine einzige Nachrichtenquelle zu verlassen."
+
+        ]
+    },
+];
