04a27f60280b8ea17b6e08baefecccdd27f2578e
pady-ai <patricia.birkenmeier@stud.fhgr.ch>
2024-02-07 11:25:38 +0100

    Korrektur und Identifikation des Medienkompetenz Quizzes

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-SCRIPT.JS
-
-
 const startBtn = document.querySelector('.start-btn'); 
 const popupInfo = document.querySelector('.popup-info'); 
 const exitBtn = document.querySelector('.exit-btn'); 
@@ -26,6 +23,7 @@ continueBtn.onclick = () => {
     quizBox.classList.add('active');
 
     showQuestions(0); 
+    questionCounter(1);
 }
     nextBtnBtn.onclick = () => {
         questionCount++;
@@ -37,12 +35,38 @@ let questionCount = 0;
 const nextBtn = document.querySelector('.next-btn');
 
 nextBtn.onclick = () => {
-    questionCount++;
+   if (questionCount < questions.length - 1) {
+   questionCount++;
     showQuestions(questionCount);
+
+    questionNumb++; 
+    questionCounter(questionNumb);
+   }
+   else {
+    console.log('Question Completed')
+   }
 }
+
+const optionList = document.querySelector('.option-list');
 
 function showQuestions(index) {
     const questionText = document.querySelector('.question-text');
-    questionText.textContent = '${question[index].numb}. ${question[index].question}';
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
 
+    let optionTag = `<div class="option"> <span>${questions[index].options[0]}</span></div>
+        <div class="option"> <span>${questions[index].options[1]}</span></div>
+        <div class="option"> <span>${questions[index].options[2]}</span></div>
+        <div class="option"> <span>${questions[index].options[3]}</span></div>`;
+
+    optionList.innerHTML = optionTag; 
+
+    const option = documen.querySelectorAll('.option');
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute('onclick', 'optionSelected(this)');
+    }
+}
+
+function questionCounter(index) {
+    const questionTotal = document.querySelector('.question-total');
+    questionTotal.textContent = `${index} of ${questions.length} Questions`;
 }
