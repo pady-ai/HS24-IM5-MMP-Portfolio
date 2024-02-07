bdeacfbb669082e15db1dbb62c6f6523f75eda23
pady-ai <patricia.birkenmeier@stud.fhgr.ch>
2024-02-07 07:58:56 +0100

    Quiz anpassen

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const startBtn = document.querySelector('.start-btn');
 const popupInfo = document.querySelector('.popup-info'); 
 const exitBtn = document.querySelector('.exit-btn'); 
 const main = document.querySelector('.main'); 
+const continueBtn = document.querySelector('.continue-btn'); 
+const quizSection = document.querySelector('.quiz-section'); 
 
 startBtn.onclick = () => {
     popupInfo.classList.add('active'); 
@@ -10,4 +12,11 @@ startBtn.onclick = () => {
 
 exitBtn.onclick = () => {
     popupInfo.classList.remove('active'); 
+    main.classList.remove('active'); 
+}
+
+continueBtn.onclick = () => {
+    quizSection.classList.remove('active'); 
+    popupInfo.classList.remove('active'); 
+    main.classList.remove('active'); 
 }
