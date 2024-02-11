e77eb78fabd560c0dff98d2dfe74966cc98c7b69
pady-ai <patricia.birkenmeier@stud.fhgr.ch>
2024-02-11 21:39:59 +0100

    Überarbeitung Fragen und Anworten

--- fragen.js
+++ fragen.js
@@ -1,57 +1,57 @@
 let questions = [
     {
         numb: 1,
-        question: "What does HTML stand for?",
-        answer: "C. Hyper Text Markup Language",
+        question: "Welche der folgenden Aussagen beschreibt Fake News am besten?",
+        answer: "C. Unwahre oder irreführende Informationen, die absichtlich verbreitet werden, um die Meinung der Leser zu beeinflussen.",
         options: [
-            "A. Hyper Type Multi Language",
-            "B. Hyper Text Multiple Language",
-            "C. Hyper Text Markup Language",
-            "D. Home Text Multi Language"
+            "A. Nachrichten, die von renommierten Journalisten veröffentlicht wurden.",
+            "B. Faktenbasierte Berichterstattung, die von mehreren Quellen überprüft wurde.",
+            "C. Unwahre oder irreführende Informationen, die absichtlich verbreitet werden, um die Meinung der Leser zu beeinflussen.",
+            "D. Artikel, die ausschließlich auf persönlichen Meinungen basieren."
         ]
     },
     {
         numb: 2,
-        question: "What does CSS stand for?",
-        answer: "A. Cascading Style Sheet",
+        question: "Was bedeutet es, Quellenkritik auszuüben?",
+        answer: "B. Die Glaubwürdigkeit von Nachrichtenquellen zu hinterfragen und verschiedene Quellen zu prüfen.",
         options: [
-            "A. Cascading Style Sheet",
-            "B. Cute Style Sheet",
-            "C. Computer Style Sheet",
-            "D. Codehal Style Sheet"
+            "A. Blindlings jeder Information zu glauben, die man im Internet findet.",
+            "B. Die Glaubwürdigkeit von Nachrichtenquellen zu hinterfragen und verschiedene Quellen zu prüfen.",
+            "C. Alle Informationen in den sozialen Medien zu teilen, ohne sie zu überprüfen.",
+            "D. Nachrichten nur aus vertrauenswürdigen Quellen zu konsumieren."
         ]
     },
     {
         numb: 3,
-        question: "What does PHP stand for?",
-        answer: "A. Hypertext Preprocessor",
+        question: "Welche der folgenden Maßnahmen trägt am meisten zur Förderung von Medienkompetenz bei Kindern bei??",
+        answer: "B. Eltern und Lehrer ermutigen, mit Kindern über Medieninhalte zu diskutieren und kritisch zu reflektieren.",
         options: [
-            "A. Hypertext Preprocessor",
-            "B. Hometext Programming",
-            "C. Hypertext Preprogramming",
-            "D. Programming Hypertext Preprocessor"
+            "A. Unkontrollierten Zugang zu allen verfügbaren Medieninhalten ermöglichen.",
+            "B. Eltern und Lehrer ermutigen, mit Kindern über Medieninhalte zu diskutieren und kritisch zu reflektieren.",
+            "C. Kinder dazu ermutigen, ausschließlich auf Unterhaltungsprogramme zuzugreifen",
+            "D. Kinder dazu ermutigen, sich ausschließlich auf eine einzige Nachrichtenquelle zu verlassen."
         ]
     },
     {
         numb: 4,
-        question: "What does SQL stand for?",
-        answer: "D. Structured Query Language",
+        question: "Was ist ein wichtiges Merkmal von Clickbait?",
+        answer: "B. Sensationslüsterne Überschriften oder Vorschaubilder, die darauf abzielen, die Neugier der Leser zu wecken.",
         options: [
-            "A. Strength Query Language",
-            "B. Stylesheet Query Language",
-            "C. Science Question Language",
-            "D. Structured Query Language"
+            "A. Objektive und ausgewogene Berichterstattung.",
+            "B. Sensationslüsterne Überschriften oder Vorschaubilder, die darauf abzielen, die Neugier der Leser zu wecken.",
+            "C. Verifizierte Informationen von seriösen Quellen.",
+            "D. Inhalte, die ausschließlich auf wissenschaftlichen Studien basieren."
         ]
     },
     {
         numb: 5,
-        question: "What does XML stand for?",
+        question: "B. Um sicherzustellen, dass nur Freunde und vertrauenswürdige Personen Zugriff auf persönliche Informationen haben.",
         answer: "D. Extensible Markup Language",
         options: [
-            "A. Excellent Multiple Language",
-            "B. Explore Multiple Language",
-            "C. Extra Markup Language",
-            "D. Extensible Markup Language"
+            "A. Weil soziale Medien immer öffentlich sind und keine Privatsphäre bieten.",
+            "B. Um sicherzustellen, dass nur Freunde und vertrauenswürdige Personen Zugriff auf persönliche Informationen haben.",
+            "C. Weil die Privatsphäre-Einstellungen keine Auswirkungen auf den Datenschutz haben.",
+            "D. Um sicherzustellen, dass die Beiträge viral werden und eine größere Reichweite erzielen."
         ]
     }
 ];
